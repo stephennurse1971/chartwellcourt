f9cfcb6a0d26368c13c120cee21f4195c213f084
stephen nurse <nurse_stephen@hotmail.com>
2024-10-06 19:11:15 +0300

    Instructions

---
+++ src/Form/InstructionsType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Instructions;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InstructionsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('topic', \Symfony\Component\Form\Extension\Core\Type\TextType::class, [
+                'required' => false,
+                'label' => 'Topic'
+            ])
+            ->add('summary')
+            ->add('media', FileType::class, [
+                'label' => 'Media',
+                'mapped' => false,
+                'required' => false
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Instructions::class,
+        ]);
+    }
+}
